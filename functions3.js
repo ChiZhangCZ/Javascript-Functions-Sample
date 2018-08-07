c950e0fa8d6a43e579687c22ce50c99ae9a4ef76
ChiZhangCZ <chi.zhang@academytrainee.com>
2018-08-07 09:47:25 +0100

    Up to function3/events2 commit

---
+++ functions3.js
@@ -0,0 +1,9 @@
+function makePerson(name, age, occupation){
+
+	var blankPerson = {name: "Blank", age: 0, occupation: "Blank"};
+	blankPerson.name = name;
+	blankPerson.age = age;
+	blankPerson.occupation = occupation;
+	return blankPerson;
+}
+
