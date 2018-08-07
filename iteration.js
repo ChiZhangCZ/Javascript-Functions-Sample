27254db852af8f9550f6641fd8f6cc7b7f440824
ChiZhangCZ <chi.zhang@academytrainee.com>
2018-08-07 14:35:50 +0100

    Iteration 4 commit

--- iteration.js
+++ iteration.js
@@ -18,16 +18,41 @@ function fizzBuzz(){
 
 	for(i=1; i<=100; i++){
 		if(i%3 == 0 && i%5 ==0){
-			console.log(i+": FizzBuzz");
+			console.log("FizzBuzz");
 		}
-		else if(i%3 == 0 && i%5 != 0){
-			console.log(i+": Fizz");
+		else if(i%3 == 0){
+			console.log("Fizz");
 		}
-		else if(i%5 == 0 && i%3 != 0){
-			console.log(i+": Buzz");
+		else if(i%5 == 0){
+			console.log("Buzz");
 		}
 		else{
-			console.log(i.toString());
+			console.log(i);
 		}
 	}
+}
+
+function threeDivide(number){
+
+	var count = 0;
+	var tempNum = number;
+
+	while(tempNum!=1){
+		if(tempNum%3 == 0){
+			tempNum = tempNum/3;
+			count++;
+			console.log(tempNum*3 + "/3 = " + tempNum);
+		}
+		else if((tempNum + 1)%3 == 0){
+			tempNum++;
+			count++;
+			console.log(tempNum - 1 + " + 1 = " + tempNum);
+		}
+		else if((tempNum - 1)%3 == 0){
+			tempNum -= 1;
+			count++;
+			console.log(tempNum + 1 + " - 1 = " + tempNum);
+		}
+	}
+	console.log("Number of iterations: " + count);
 }
