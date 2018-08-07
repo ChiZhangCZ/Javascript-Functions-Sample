cfd35c322358ef78c79f499b919281a08f9de778
ChiZhangCZ <chi.zhang@academytrainee.com>
2018-08-07 11:55:20 +0100

    Level 1 complete commit

---
+++ conditionals1.js
@@ -0,0 +1,9 @@
+function ageCheck(person){
+	
+	if(person.age > 20 && person.age < 40){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
---
+++ iteration.js
@@ -0,0 +1,15 @@
+function oneToTen(){
+	
+	for(i=1;  i <= 10 ;i++){
+		console.log(i);
+	}
+}
+
+function  oneToTenEvens(){
+
+	for(i=1;  i <= 10 ;i++){
+		if(i%2 == 0){
+		console.log(i);
+	    }
+	}
+}
